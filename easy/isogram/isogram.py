4b898b13c1e0b70269227f52a764784f5fccfee2
danielle <75538982+docopeland@users.noreply.github.com>
2021-01-01 18:54:15 -0500

    Add files via upload

--- easy/isogram/isogram.py
+++ easy/isogram/isogram.py
@@ -1,10 +1,3 @@
 def is_isogram(string):
-    listicle=[]
-    listicle[:0] = string.lower()
-    if " " in listicle: listicle.remove(" ")
-    if "-" in listicle: listicle.remove("-")
-    bool = True
-    for letter in listicle:
-        if listicle.count(letter) > 1: return False
-        else: continue
-    return bool
+    string = string.lower().replace(" ","").replace("-","")
+    return len(string) == len(set(string))
