0fbc39a7461b326b5a4eb5f30ff16585a65454fe
danielle <75538982+docopeland@users.noreply.github.com>
2021-02-07 22:42:58 -0500

    v2

--- easy/largest-series-product/largest_series_product.py
+++ easy/largest-series-product/largest_series_product.py
@@ -1,12 +1,7 @@
+from math import prod
+
 def largest_product(series, size):
     if size > len(series) or size < 0:
         raise ValueError("Size out of range")
-    else:
-        val = None
-        for i in range(len(series)-size+1):
-            temp = 1
-            for num in series[i:i+size]:
-                temp = temp * int(num)
-            if val is None or temp > val:
-                val = temp
-        return val
+    nums = [int(x) for x in series]
+    return max(prod(nums[i:i+size]) for i in range(0,len(series)-size+1))
