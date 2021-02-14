47283d29c4d047388db3e7b5081c9ca39097762c
danielle <75538982+docopeland@users.noreply.github.com>
2021-02-14 01:03:22 -0500

    v2

--- easy/run-length-encoding/run_length_encoding.py
+++ easy/run-length-encoding/run_length_encoding.py
@@ -2,27 +2,35 @@ import re
 
 def decode(string):
     dec = ""
-    for i in range(len(string)):
-        if re.match("[1-9]"):
-            dec = dec + int(string[i])*string[i+1]
+    decString = string
+    while len(decString) > 0:
+        if re.match("[0-9]+",decString):
+            num = int(re.match("[0-9*]+",decString).group()) - 1
+            start = re.match("[0-9*]+",decString).span()[1]
+            dec = dec + num*decString[start]
+            decString = decString[start:]
         else:
-            continue
+            dec = dec + decString[0]
+            decString = decString[1:]
     return dec
-
-
-
-
 
 def encode(string):
-    dec = ""
+    enc = ""
     count = 1
-    for i in range(1,len(string)):
-        if string[i-1] == string[i]:
-            count = count + 1
+    for i in range(len(string)):
+        if i + 1 < len(string):
+            if string[i] == string[i+1]:
+                count = count + 1
+                continue
+            else:
+                if count == 1:
+                    enc = enc + string[i]
+                else:
+                    enc = enc + str(count) + string[i]
+                    count = 1
         else:
             if count == 1:
-                dec = dec + string[i-1]
-            else:    
-                dec = dec + str(count)+string[i-1]
-            count = 1
-    return dec
+                enc = enc + string[i]
+            else:
+                enc = enc + str(count) + string[i]
+    return enc
