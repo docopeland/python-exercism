111d321c9f9d53775e4e7d6cd00fec8afdca2d72
danielle <75538982+docopeland@users.noreply.github.com>
2021-02-15 23:17:01 -0500

    v3

--- easy/run-length-encoding/run_length_encoding.py
+++ easy/run-length-encoding/run_length_encoding.py
@@ -1,36 +1,7 @@
 import re
 
 def decode(string):
-    dec = ""
-    decString = string
-    while len(decString) > 0:
-        if re.match("[0-9]+",decString):
-            num = int(re.match("[0-9*]+",decString).group()) - 1
-            start = re.match("[0-9*]+",decString).span()[1]
-            dec = dec + num*decString[start]
-            decString = decString[start:]
-        else:
-            dec = dec + decString[0]
-            decString = decString[1:]
-    return dec
+    return re.sub(r"(\d+)(\D)",lambda x: x.group(2) * int(x.group(1)),string)
 
 def encode(string):
-    enc = ""
-    count = 1
-    for i in range(len(string)):
-        if i + 1 < len(string):
-            if string[i] == string[i+1]:
-                count = count + 1
-                continue
-            else:
-                if count == 1:
-                    enc = enc + string[i]
-                else:
-                    enc = enc + str(count) + string[i]
-                    count = 1
-        else:
-            if count == 1:
-                enc = enc + string[i]
-            else:
-                enc = enc + str(count) + string[i]
-    return enc
+    return re.sub(r"(.)\1+",lambda x: str(len(x.group(0))) + x.group(1),string)
