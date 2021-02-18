08303cef5f4556caf03b666b05665a13edf31e65
danielle <75538982+docopeland@users.noreply.github.com>
2021-02-18 00:09:03 -0500

    atbash v3

--- easy/atbash-cipher/atbash_cipher.py
+++ easy/atbash-cipher/atbash_cipher.py
@@ -1,28 +1,14 @@
-alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-cipher = ["z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a"]
+cipher = str.maketrans("abcdefghijklmnopqrstuvwxyz","zyxwvutsrqponmlkjihgfedcba")
 
-def encode(plain_text):
-    text = ""
-    for letter in plain_text.lower():
-        if letter in alpha:
-            abc = alpha.index(letter)
-            text = text + cipher[abc]
-        elif letter.isdigit():
-            text = text + letter
-        else:
-            continue
-    for num in range(len(text)-1,-1,-1):
-        if num % 5 == 0 and num != 0:
-            text = text[:num] + " " + text[num:]
-    return text
+def encode(plain_text: str):
+    text = "".join([letter for letter in plain_text if letter.isalnum()])
+    return spacing("".join([letter.translate(cipher) for letter in text.lower() if letter.isalnum()]))
+
+def spacing(text:str):
+    if len(text) < 6:
+        return text
+    else:
+        return text[:5] + " " + spacing(text[5:])
 
 def decode(ciphered_text):
-    text = ""
-    for letter in ciphered_text.lower():
-        if letter in alpha:
-            text = text + alpha[cipher.index(letter)]
-        elif letter.isdigit():
-            text = text + letter
-        else:
-            continue
-    return text
+    return "".join([letter.translate(cipher) for letter in ciphered_text.lower() if letter.isalnum()])
