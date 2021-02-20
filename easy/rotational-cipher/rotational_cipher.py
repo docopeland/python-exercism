0462b1c66d62fca899fccc735f014cac43561d02
danielle <75538982+docopeland@users.noreply.github.com>
2021-02-19 23:37:58 -0500

    rotate v2

--- easy/rotational-cipher/rotational_cipher.py
+++ easy/rotational-cipher/rotational_cipher.py
@@ -1,5 +1,5 @@
 def rotate(text:str, key:int):
     abc = "abcdefghijklmnopqrstuvwxyz"
     cipher = abc[key:] + abc[:key]
-    trans = str.maketrans(abc,cipher)
-    return "".join([letter.translate(trans) for letter in text if letter.isalpha()])
+    trans = str.maketrans(abc + abc.upper(),cipher + cipher.upper())
+    return str.translate(text,trans)
