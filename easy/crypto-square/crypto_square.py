604293799b929d546c2091e740acd97b15e249ae
danielle <75538982+docopeland@users.noreply.github.com>
2021-02-23 02:01:55 -0500

    crypto v2

--- easy/crypto-square/crypto_square.py
+++ easy/crypto-square/crypto_square.py
@@ -1,12 +1,17 @@
-from math import sqrt, pow
+from math import sqrt, ceil, floor
 
 def cipher_text(plain_text):
-    text = [val for val in plain_text if val.isalnum()]
-    if sqrt(len(text)) % 1 == 0:
-        num = len(text)
-    else:
-        for val in range(len(text),len(text)*2):
-            if sqrt(val) % 1 == 0:
-                num = val
-                break
-    
+    text = "".join([val for val in plain_text.lower() if val.isalnum()])
+    c = ceil(sqrt(len(text)))
+    r = round(sqrt(len(text)))
+    l = ["" for i in range(c)]
+    for i in range(r):
+        for j in range(c):
+            try:
+                l[j] = l[j] + text[i*c + j]
+            except IndexError:
+                l[j] = l[j] + " "
+    for i in range(len(l)):
+        if len(l)>0 and i+1 < len(l):
+            l[i] = l[i] + " "
+    return "".join(l)
