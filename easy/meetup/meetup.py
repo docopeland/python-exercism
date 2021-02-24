3e956c72a772a91f093540f93776337bcd43e034
danielle <75538982+docopeland@users.noreply.github.com>
2021-02-24 00:16:47 -0500

    meetup v2

--- easy/meetup/meetup.py
+++ easy/meetup/meetup.py
@@ -1,16 +1,19 @@
 from datetime import date
-
+from calendar import monthrange
 
 def meetup(year:int, month:int, week, day_of_week):
     weeks = {"teenth":range(13,20),"1st":range(1,8),"2nd":range(8,15),"3rd":range(15,22),"4th":range(22,29),
-             "5th":range(29,32),"last":(21,32)}
+             "5th":range(29,32),"last":range(31,20,-1)}
     weekday = {"Monday":0,"Tuesday":1,"Wednesday":2,"Thursday":3,"Friday":4,"Saturday":5,"Sunday":6}
-    if week in weeks:
-        days = weeks.get(week)
-        for day in days:
-            print(year,month,day,day_of_week,date(year, month, day).weekday())
-            if date(year, month, day).weekday() == weekday.get(day_of_week):
-                return date(year, month, day)
+    dt = date.today()
+    for day in weeks.get(week):
+        if monthrange(year,month)[1] >= day and date(year, month, day).weekday() == weekday.get(day_of_week):
+            dt = date(year, month, day)
+            break
+    if dt != date.today():
+        return dt
+    else:
+        raise MeetupDayException("this does not exist")
 
-def MeetupDayException():
-    raise MeetupDayException()
+class MeetupDayException(Exception):
+    pass
