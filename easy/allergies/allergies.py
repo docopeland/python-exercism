612c38817d9897cad21e0de8619ed89f4942d582
danielle <75538982+docopeland@users.noreply.github.com>
2021-03-02 00:11:51 -0500

    allergies v3

--- easy/allergies/allergies.py
+++ easy/allergies/allergies.py
@@ -1,24 +1,12 @@
 class Allergies:
 
-    self.allergies = ["eggs","peanuts","shellfish","strawberries","tomatoes","chocolate","pollen","cats"]
-
     def __init__(self, score):
         self.score = score
-        self.vals = {1: "eggs",2:"peanuts",4:"shellfish",8:"strawberries",16:"tomatoes",32:"chocolate",64:"pollen",
-                     128:"cats"}
+        self.allergies = ["eggs","peanuts","shellfish","strawberries","tomatoes","chocolate","pollen","cats"]
 
     def allergic_to(self, item,score=None):
-        scoring = score if score is not None else self.score
-        if scoring < 1:
-            return False
-        for key,value in sorted(self.vals.items(),key = lambda x: x[0],reverse=True):
-            if item == value and scoring >= key:
-                return True
-            elif scoring >= key:
-                return self.allergic_to(item,scoring-key)
+        return item in self.lst
 
     @property
     def lst(self):
-        allergies = []
-        for index,value in enumerate(self.allergies):
-            if 2**index
+        return [value for index,value in enumerate(self.allergies) if 2**index & self.score]
