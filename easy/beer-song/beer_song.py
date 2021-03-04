96d22393c55e6f77aa903132ce3c0dd01dd6c1e7
danielle <75538982+docopeland@users.noreply.github.com>
2021-03-03 23:56:42 -0500

    beer v2

--- easy/beer-song/beer_song.py
+++ easy/beer-song/beer_song.py
@@ -1,21 +1,17 @@
 def recite(start, take=1):
     wallBeers = []
     while take >= 1:
-        num = start
-        bottles = "bottles"
-        if start == 1:
-            bottles = "bottle"
-        if start == 0:
-            num = "no more"
-        wallBeers.append("{} {} of beer on the wall, {} {} of beer.".format(num,bottles,num,bottles).capitalize())
-        if num == 2:
-            bottles = "bottle"
-        if num == "no more":
+        s1 = "s" if start != 1 else ""
+        s2 = "s" if start != 2 else ""
+        num1 = start if start != 0 else "no more"
+        num2 = "no more" if num1 == "no more" or num1 == 1 else num1 - 1
+        down = "one" if num1 != 1 else "it"
+        
+        wallBeers.append("{} bottle{} of beer on the wall, {} bottle{} of beer.".format(num1,s1,num1,s1).capitalize())
+        if num1 == "no more":
             wallBeers.append("Go to the store and buy some more, 99 bottles of beer on the wall.")
-        if num == 1:
-            wallBeers.append("Take it down and pass it around, no more bottles of beer on the wall.")
-        if num != "no more" and start > 1:
-            wallBeers.append("Take one down and pass it around, {} {} of beer on the wall.".format(start-1,bottles))
+        else:
+            wallBeers.append("Take {} down and pass it around, {} bottle{} of beer on the wall.".format(down,num2,s2))
         start = start - 1
         take = take - 1
         if take > 0:
