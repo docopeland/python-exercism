64c328628a9da0ba22607f58a44655b1733a511c
danielle <75538982+docopeland@users.noreply.github.com>
2021-03-04 00:02:32 -0500

    beer v3

--- easy/beer-song/beer_song.py
+++ easy/beer-song/beer_song.py
@@ -4,14 +4,15 @@ def recite(start, take=1):
         s1 = "s" if start != 1 else ""
         s2 = "s" if start != 2 else ""
         num1 = start if start != 0 else "no more"
-        num2 = "no more" if num1 == "no more" or num1 == 1 else num1 - 1
-        down = "one" if num1 != 1 else "it"
-        
-        wallBeers.append("{} bottle{} of beer on the wall, {} bottle{} of beer.".format(num1,s1,num1,s1).capitalize())
         if num1 == "no more":
-            wallBeers.append("Go to the store and buy some more, 99 bottles of beer on the wall.")
-        else:
-            wallBeers.append("Take {} down and pass it around, {} bottle{} of beer on the wall.".format(down,num2,s2))
+            num2 = 99
+        elif num1 == 1:
+            num2 = "no more"
+        else: num2 = num1 - 1
+        down = "one" if num1 != 1 else "it"
+        sen1 = "Take {} down and pass it around".format(down) if num1 != "no more" else "Go to the store and buy some more"
+        wallBeers.append("{} bottle{} of beer on the wall, {} bottle{} of beer.".format(num1,s1,num1,s1).capitalize())
+        wallBeers.append("{}, {} bottle{} of beer on the wall.".format(sen1,num2,s2))
         start = start - 1
         take = take - 1
         if take > 0:
