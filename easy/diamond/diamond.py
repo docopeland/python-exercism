289eadf6ed662aee7315958e941169beda5d9bef
danielle <75538982+docopeland@users.noreply.github.com>
2021-03-06 00:10:56 -0500

    diamond v3

--- easy/diamond/diamond.py
+++ easy/diamond/diamond.py
@@ -1,19 +1,14 @@
 import string
 
 def rows(letter):
-    abc = enumerate(string.ascii_uppercase)
-    indx = sum([key for key,val in abc if letter == val])*2-1
     val = []
-    upto = string.ascii_uppercase.index(letter)
-    fullabc = string.ascii_uppercase[:upto+1]+"".join(reversed(string.ascii_uppercase[:upto]))
+    indx = string.ascii_uppercase.index(letter)
+    fullabc = string.ascii_uppercase[:indx+1]+"".join(reversed(string.ascii_uppercase[:indx]))
     for let in fullabc:
-        if let > letter:
-            continue
+        inside = sum([key for key,val in enumerate(string.ascii_uppercase) if let == val])*2-1
+        outside = int((len(fullabc)-inside-2)/2)
+        if let != "A":
+            val = val + [(outside*" ")+let+(inside*" ")+let+(outside*" ")]
         else:
-            inside = sum([key for key,val in enumerate(string.ascii_uppercase) if let == val])*2-1
-            outside = int((indx - inside)/2) if let != "A" else int((indx - inside)/2)
-            if let != "A":
-                val = val + [(outside*" ")+let+(inside*" ")+let+(outside*" ")]
-            else:
-                val = val + [(outside*" ")+let+(outside*" ")]
+            val = val + [(outside*" ")+let+(outside*" ")]
     return val
