c4568a3f9c5a3b9239378b2a180336d244f5517e
danielle <75538982+docopeland@users.noreply.github.com>
2021-03-16 23:22:27 -0400

    binary v2

--- easy/binary-search/binary_search.py
+++ easy/binary-search/binary_search.py
@@ -1,5 +1,10 @@
-def find(search_list:list, value):
-    if value in search_list:
-        return search_list.index(value)
-    else:
-        raise ValueError("Not in list")
+def find(search_list:list, value,indx=0):
+    half = len(search_list)//2
+    if len(search_list) < 1 or (len(search_list) == 1 and search_list[half] != value):
+        raise ValueError("Not in series")
+    elif search_list[half] == value:
+        return indx + half
+    elif search_list[half] > value:
+        return find(search_list[:half],value,indx)
+    elif search_list[half] < value:
+        return find(search_list[half:],value,half+indx)
